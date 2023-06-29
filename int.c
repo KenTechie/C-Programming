db0fa305672d2144298da5cf0c93a29374fb017f
KenTechie <120584109+KenTechie@users.noreply.github.com>
2023-06-29 18:47:28 +0300

    New commit

---
+++ int.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main() {
+    int number;
+    printf("Enter an integer:25 ");
+    // reads and stores input
+    scanf("%d", &number);
+    // displays output 
+    printf("You entered: %d", number);
+    return 0;
+}
